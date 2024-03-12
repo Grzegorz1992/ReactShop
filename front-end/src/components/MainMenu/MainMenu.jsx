66bd6e56c33b18303b2cf2fa8dfdbe3f3de69bf3
Grzegorz <gkopacki92@gmail.com>
2024-03-12 13:50:40 +0100

    created MainMenu component

---
+++ front-end/src/components/MainMenu/MainMenu.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom";
+import styles from "./MainMenu.module.css"
+
+export function MainMenu() {
+	const genders = [
+		{
+			categoryName: "Kobieta",
+			path: "kobieta",
+		},
+		{
+			categoryName: "Mężczyzna",
+			path: "mezczyzna",
+		},
+		{
+			categoryName: "Dziecko",
+			path: "dziecko",
+		},
+	];
+	return (
+		<ul className={styles.mainMenu}>
+			{genders.map((category) => {
+				return (
+					<li key={category.path}>
+						<NavLink to={category.path}>{category.categoryName}</NavLink>
+					</li>
+				);
+			})}
+		</ul>
+	);
+}
