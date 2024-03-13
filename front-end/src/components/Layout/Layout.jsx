383ec88421719f403cab171269c5e82d5b003c32
Grzegorz <gkopacki92@gmail.com>
2024-03-13 23:50:23 +0100

    created TopBar component

--- front-end/src/components/Layout/Layout.jsx
+++ front-end/src/components/Layout/Layout.jsx
@@ -3,14 +3,20 @@ import { Footer } from "../Footer/Footer";
 import { Logo } from "../Logo/Logo";
 import { MainMenu } from "../MainMenu/MainMenu";
 import { IconMenu } from "../IconMenu/IconMenu";
+import { TopBar } from "../TopBar/TopBar";
 
 export function Layout() {
 	return (
 		<>
-			<MainMenu />
-			<Logo />
-			<CurrencySelector />
-			<IconMenu />
+			<TopBar>
+				<MainMenu />
+				<Logo />
+				<div>
+					<CurrencySelector />
+					<IconMenu />
+				</div>
+			</TopBar>
+
 			<Footer />
 		</>
 	);
