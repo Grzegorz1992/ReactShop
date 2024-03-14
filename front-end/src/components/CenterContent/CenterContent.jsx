d1b6747b00e8558a0778ac3756e33bac8d74dc99
Grzegorz <gkopacki92@gmail.com>
2024-03-14 21:45:26 +0100

    created Hero component

--- front-end/src/components/CenterContent/CenterContent.jsx
+++
@@ -1,4 +0,0 @@
-import styles from "./CenterContent.module.css";
-export function CenterContent({ children }) {
-	return <div className={styles.centerContent}>{children}</div>;
-}
--- front-end/src/views/MainPage/MainPage.jsx
+++ front-end/src/views/MainPage/MainPage.jsx
@@ -1,4 +1,9 @@
+import { Hero } from "../../components/Hero/Hero";
 import { Layout } from "../../components/Layout/Layout";
 export function MainPage() {
-	return <Layout />;
+	return (
+		<Layout>
+			<Hero />
+		</Layout>
+	);
 }
