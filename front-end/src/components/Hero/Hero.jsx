45b15fe7cf84b7fc4a20577f0e9eaeb9bccff960
Grzegorz <gkopacki92@gmail.com>
2024-03-14 21:53:37 +0100

    added hero jpg for Hero component

--- front-end/src/components/Hero/Hero.jsx
+++ front-end/src/components/Hero/Hero.jsx
@@ -1,8 +1,12 @@
 import { CenteredContent } from "../CenteredContent/CenteredContent";
+import styles from "./Hero.module.css";
 
-export function Hero() {
+export function Hero({ heroImage }) {
 	return (
-		<div>
+		<div
+			className={styles.hero}
+			style={{ backgroundImage: `url(${heroImage})` }}
+		>
 			<CenteredContent>
 				<div>
 					<h2>Letnie promocje do -70%</h2>
--- front-end/src/views/MainPage/MainPage.jsx
+++ front-end/src/views/MainPage/MainPage.jsx
@@ -1,9 +1,10 @@
 import { Hero } from "../../components/Hero/Hero";
 import { Layout } from "../../components/Layout/Layout";
+import COVER_PHOTO from "../../assets/hero.jpg";
 export function MainPage() {
 	return (
 		<Layout>
-			<Hero />
+			<Hero heroImage={COVER_PHOTO} />
 		</Layout>
 	);
 }
