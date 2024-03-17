121096d41dbde87bfd8c788bcd22ffed5f3cfec9
Grzegorz <gkopacki92@gmail.com>
2024-03-17 17:40:09 +0100

    created ProductList view

---
+++ front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
@@ -0,0 +1,35 @@
+import styles from "./ExpandableMenu.module.css";
+import { NavLink } from "react-router-dom";
+import { CATEGORIES } from "../../constants/categories";
+import ARROW_ICON from "../../assets/arrow.svg";
+
+export function ExpandableMenu() {
+	return (
+		<div className={styles.expandableMenu}>
+			<p>Kobieta</p>
+			<ul>
+				{CATEGORIES.map((category) => {
+					return (
+						<li key={category.path}>
+							<NavLink to={category.path}>
+								{category.categoryName}
+								<img src={ARROW_ICON} />
+							</NavLink>
+							<ul>
+								{category.subcategories.map((subcategory) => {
+									return (
+										<li key={subcategory.path}>
+											<NavLink to={subcategory.path}>
+												{subcategory.categoryName}
+											</NavLink>
+										</li>
+									);
+								})}
+							</ul>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+}
