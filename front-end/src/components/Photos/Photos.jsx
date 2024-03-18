3e70a1e92ae636a57ca18365befd127d450126f8
Grzegorz <gkopacki92@gmail.com>
2024-03-18 09:53:29 +0100

    created Photos component

--- front-end/src/components/Photos/Photos.jsx
+++ front-end/src/components/Photos/Photos.jsx
@@ -1,4 +1,25 @@
-import styles from "./Photos.module.css"
-export function Photos() {
-
+import styles from "./Photos.module.css";
+import { FlexContainer } from "../../components/FlexContainer/FlexContainer";
+import { useState } from "react";
+export function Photos({ product }) {
+	const [currentPhoto, setCurrentPhoto] = useState(product.photos[0]);
+	return (
+		<FlexContainer>
+			<div className={styles.thumbnails}>
+				{product.photos.map((photo) => {
+					return (
+						<img
+							className={currentPhoto === photo ? styles.active : ""}
+							key={photo}
+							src={photo}
+							onClick={() => {
+								setCurrentPhoto(photo);
+							}}
+						/>
+					);
+				})}
+			</div>
+			<img className={styles.mainPhoto} src={currentPhoto} alt="" />
+		</FlexContainer>
+	);
 }
