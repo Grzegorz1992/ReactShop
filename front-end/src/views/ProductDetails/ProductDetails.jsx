8e4158b7b9648a1b553dd459734de938f95f6ed6
Grzegorz <gkopacki92@gmail.com>
2024-03-18 09:37:53 +0100

    added Layout for ProductDetails view

--- front-end/src/views/ProductDetails/ProductDetails.jsx
+++ front-end/src/views/ProductDetails/ProductDetails.jsx
@@ -1,4 +1,14 @@
 import { Layout } from "../../components/Layout/Layout";
+import { ExpandableMenu } from "../../components/ExpandableMenu/ExpandableMenu";
+import { Breadcrumbs } from "../../components/Breadcrumbs/Breadcrumbs";
+import { FlexContainer } from "../../components/FlexContainer/FlexContainer";
 export function ProductDetails() {
-	return <Layout></Layout>;
+	return (
+		<Layout>
+			<FlexContainer>
+				<ExpandableMenu />
+				<Breadcrumbs />
+			</FlexContainer>
+		</Layout>
+	);
 }
