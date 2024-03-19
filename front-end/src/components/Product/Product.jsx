788ee6e9f2ea12a7fd0cb1429dc9fdeff0156144
Grzegorz <gkopacki92@gmail.com>
2024-03-19 22:56:01 +0100

    added handle for data loader in product comoponent

--- front-end/src/components/Product/Product.jsx
+++ front-end/src/components/Product/Product.jsx
@@ -1,9 +1,20 @@
 import { Link } from "react-router-dom";
 import styles from "./Product.module.css";
 
+const ENDPOINT_TO_PATH_MAPPING = {
+	men: "mezczyzna",
+	women: "kobieta",
+	children: "dziecko",
+};
+
 export function Product({ product }) {
 	return (
-		<Link className={styles.product}>
+		<Link
+			className={styles.product}
+			to={`/${ENDPOINT_TO_PATH_MAPPING[product.gender]}/${product.category}/${
+				product.subcategory
+			}/${product.id}`}
+		>
 			<img src={product.photos[0]} />
 			<h3>{product.productName}</h3>
 			<p>{product.pricePLN}zł</p>
