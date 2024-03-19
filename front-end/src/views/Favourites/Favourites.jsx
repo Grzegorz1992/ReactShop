ef66f126e0ae5b2d17567b8f53ad846e998843c2
Grzegorz <gkopacki92@gmail.com>
2024-03-19 10:54:10 +0100

    created path for Cart and Favourites views

--- front-end/src/views/Favourites/Favourites.jsx
+++ front-end/src/views/Favourites/Favourites.jsx
@@ -1,5 +1,5 @@
 import { FavouriteList } from "../../components/FavouriteList/FavouriteList";
-import { Layout } from "../../components/Layout/Layout";
+
 
 export function Favourites() {
 	const favouriteProducts = [
@@ -42,9 +42,5 @@ export function Favourites() {
 				"Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
 		},
 	];
-	return (
-		<Layout>
-			<FavouriteList products={favouriteProducts} />
-		</Layout>
-	);
+	return <FavouriteList products={favouriteProducts} />;
 }
