d725a9811ff0dc55e97d2f5202cc726ac8ba27fc
Grzegorz <gkopacki92@gmail.com>
2024-03-26 12:56:49 +0100

    added component Price in Details component

--- front-end/src/components/Details/Details.jsx
+++ front-end/src/components/Details/Details.jsx
@@ -6,48 +6,51 @@ import { FullWidthButton } from "../FullWidthButton/FullWidthButton";
 import { Accordion } from "../Accordion/Accordion";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
+import { Price } from "../Price/Price";
 
 export function Details({ product }) {
-    const [, addProductToCart] = useContext(CartContext);
+	const [, addProductToCart] = useContext(CartContext);
 
-    const accordionContent = [
-        {
-            title: "Opis produktu",
-            content: product.description,
-        },
-        {
-            title: "Wskazówki pielęgnacyjne",
-            content: product.maintenanceInfo,
-        },
-    ];
+	const price = <Price product={product} />;
 
-    return (
-        <div className={styles.details}>
-            <h2>{product.brand}</h2>
-            <p className={styles.productName}>{product.productName}</p>
-            <p className={styles.price}>{product.pricePLN}zł</p>
+	const accordionContent = [
+		{
+			title: "Opis produktu",
+			content: product.description,
+		},
+		{
+			title: "Wskazówki pielęgnacyjne",
+			content: product.maintenanceInfo,
+		},
+	];
 
-            <FullWidthButton
-                onClick={() => {
-                    addProductToCart(product);
-                }}
-                isBlack={true}
-            >
-                Dodaj do koszyka
-            </FullWidthButton>
+	return (
+		<div className={styles.details}>
+			<h2>{product.brand}</h2>
+			<p className={styles.productName}>{product.productName}</p>
+			<p className={styles.price}>{price}</p>
 
-            <ul className={styles.extraInfo}>
-                <li>
-                    <img src={CAR_ICON} />
-                    Dostawa do 24h
-                </li>
-                <li>
-                    <img src={RETURN_ICON} />
-                    Zwrot do 100 dni!
-                </li>
-            </ul>
+			<FullWidthButton
+				onClick={() => {
+					addProductToCart(product);
+				}}
+				isBlack={true}
+			>
+				Dodaj do koszyka
+			</FullWidthButton>
 
-            <Accordion items={accordionContent} />
-        </div>
-    );
+			<ul className={styles.extraInfo}>
+				<li>
+					<img src={CAR_ICON} />
+					Dostawa do 24h
+				</li>
+				<li>
+					<img src={RETURN_ICON} />
+					Zwrot do 100 dni!
+				</li>
+			</ul>
+
+			<Accordion items={accordionContent} />
+		</div>
+	);
 }
