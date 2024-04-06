de0955ee299a9d1575923dbd828d4cfc3fa2d9ca
Grzegorz <gkopacki92@gmail.com>
2024-04-06 20:29:58 +0200

    created new View Payment

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -16,54 +16,59 @@ import { addProductToFavourites } from "./api/addProductToFavouritesAction";
 import { favouritesLoader } from "./api/favouritesLoader";
 import { deleteFavouriteAction } from "./api/deleteFavouriteAction";
 import { Checkout } from "./views/Checkout/Checkout";
+import {Payment} from "./views/Payment/Payment"
 
 const router = createBrowserRouter([
-    {
-        path: "/add-to-favourites/:productId",
-        action: addProductToFavourites,
-    },
-    {
-        path: "/delete-from-favourites/:favouriteId",
-        action: deleteFavouriteAction,
-    },
-    {
-        path: "",
-        element: <Layout />,
-        children: [
-            {
-                path: "/koszyk",
-                element: <Cart />,
-            },
-            {
-                path: "/ulubione",
-                element: <Favourites />,
-                loader: favouritesLoader,
-            },
-            {
-                path: "/:gender?",
-                element: <MainPage />,
-                loader: mainPageLoader,
-            },
-            {
-                path: "/:gender/:category/:subcategory?",
-                element: <ProductsList />,
-                loader: productListLoader,
-            },
-            {
-                path: "/:gender/:category/:subcategory/:productId",
-                element: <ProductDetails />,
-                loader: productLoader,
-            },
-            {
-                path: "/zamowienie",
-                element: <Checkout/>
-            }
-        ],
-    },
+	{
+		path: "/add-to-favourites/:productId",
+		action: addProductToFavourites,
+	},
+	{
+		path: "/delete-from-favourites/:favouriteId",
+		action: deleteFavouriteAction,
+	},
+	{
+		path: "",
+		element: <Layout />,
+		children: [
+			{
+				path: "/koszyk",
+				element: <Cart />,
+			},
+			{
+				path: "/ulubione",
+				element: <Favourites />,
+				loader: favouritesLoader,
+			},
+			{
+				path: "/:gender?",
+				element: <MainPage />,
+				loader: mainPageLoader,
+			},
+			{
+				path: "/:gender/:category/:subcategory?",
+				element: <ProductsList />,
+				loader: productListLoader,
+			},
+			{
+				path: "/:gender/:category/:subcategory/:productId",
+				element: <ProductDetails />,
+				loader: productLoader,
+			},
+			{
+				path: "/zamowienie",
+				element: <Checkout />,
+			},
+			{
+				path: "/platnosc",
+				element: <Payment />,
+			},
+		],
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <RouterProvider router={router}></RouterProvider>
-    </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router}></RouterProvider>
+	</React.StrictMode>
 );
