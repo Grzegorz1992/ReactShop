02e137cdbf89494d8d0aaa0c8a7a0089f804bbe8
Grzegorz <gkopacki92@gmail.com>
2024-04-10 10:28:48 +0200

    styled FormPayment component

--- front-end/src/components/FormPayment/FormPayment.jsx
+++ front-end/src/components/FormPayment/FormPayment.jsx
@@ -3,72 +3,61 @@ export function FormPaymanet() {
 	return (
 		<form className={styles.form}>
 			<div>
-				<h2 className={styles.heading}>Metoda wysyłki</h2>
+				<h2 className={styles.heading}>Metoda płatności</h2>
 			</div>
 			<div className={styles.inputContainer}>
 				<input
 					className={styles.radio}
 					type="radio"
-					id="shipment"
-					name="shipment"
+					id="payment"
+					name="paymnet"
 				/>
-				<label className={styles.label} htmlFor="shipment">
-					VIAPOST
+				<label className={styles.label} htmlFor="payment">
+					PayNow
 				</label>
 			</div>
 			<div className={styles.inputContainer}>
 				<input
 					className={styles.radio}
 					type="radio"
-					id="shipment"
-					name="shipment"
+					id="payment"
+					name="paymnet"
 				/>
-				<label className={styles.label} htmlFor="shipment">
-					HAPPYDRIVE
+				<label className={styles.label} htmlFor="payment">
+					YourPay
 				</label>
 			</div>
 			<div className={styles.inputContainer}>
 				<input
 					className={styles.radio}
 					type="radio"
-					id="shipment"
-					name="shipment"
+					id="payment"
+					name="paymnet"
 				/>
-				<label className={styles.label} htmlFor="shipment">
-					FLYPACK
+				<label className={styles.label} htmlFor="payment">
+					Bank transfer
 				</label>
 			</div>
 			<div className={styles.inputContainer}>
 				<input
 					className={styles.radio}
 					type="radio"
-					id="shipment"
-					name="shipment"
+					id="payment"
+					name="paymnet"
 				/>
-				<label className={styles.label} htmlFor="shipment">
-					YOURSHIPMENT
+				<label className={styles.label} htmlFor="payment">
+					Credit card
 				</label>
 			</div>
 			<div className={styles.inputContainer}>
 				<input
 					className={styles.radio}
 					type="radio"
-					id="shipment"
-					name="shipment"
+					id="payment"
+					name="paymnet"
 				/>
-				<label className={styles.label} htmlFor="shipment">
-					DEMISEND
-				</label>
-			</div>
-			<div className={styles.inputContainer}>
-				<input
-					className={styles.radio}
-					type="radio"
-					id="shipment"
-					name="shipment"
-				/>
-				<label className={styles.label} htmlFor="shipment">
-					ODBIÓR W SKLEPIE
+				<label className={styles.label} htmlFor="payment">
+					SpeedCash
 				</label>
 			</div>
 		</form>
