135234e3a099981e8d0fae74bce20020f2eeb9d1
Grzegorz <gkopacki92@gmail.com>
2024-04-13 15:18:39 +0200

    created PaymentSummary component

---
+++ front-end/src/components/PaymentSummary/PaymentSummary.jsx
@@ -0,0 +1,73 @@
+import styles from "./PaymentSummary.module.css";
+import { FullWidthButton } from "../FullWidthButton/FullWidthButton";
+import CAR_ICON from "../../assets/car.svg";
+import { useContext } from "react";
+import { CurrencyContext } from "../../contexts/CurrencyContext";
+import { CURRENCIES, CURRENCY_SIGN } from "../../constants/currencies";
+// import { Link } from "react-router-dom";
+
+export function PaymentSummary({ products }) {
+	const [currency] = useContext(CurrencyContext);
+
+	const deliveryCosts = {
+		[CURRENCIES.USD]: 10,
+		[CURRENCIES.PLN]: 49,
+	};
+
+	const minSumsForFreeDelivery = {
+		[CURRENCIES.USD]: 100,
+		[CURRENCIES.PLN]: 500,
+	};
+
+	const currencySign = CURRENCY_SIGN[currency];
+
+	let sum = products.reduce((total, product) => {
+		return (
+			total +
+			(currency === CURRENCIES.PLN ? product.pricePLN : product.priceUSD) *
+				product.quantity
+		);
+	}, 0);
+
+	const deliveryCost = deliveryCosts[currency];
+	const minSumForFreeDelivery = minSumsForFreeDelivery[currency];
+
+	const totalCost = sum > minSumForFreeDelivery ? sum : sum + deliveryCost;
+
+	return (
+		<div className={styles.cartSummary}>
+			<h2>Podsumowanie</h2>
+			<div className={styles.cartRow}>
+				<p>Wartość produktów: </p>
+				<p>
+					{sum}
+					{currencySign}
+				</p>
+			</div>
+			<div className={styles.cartRow}>
+				<p>Koszt dostawy:</p>
+				<p>
+					{sum > minSumForFreeDelivery ? 0 : deliveryCost}
+					{currencySign}
+				</p>
+			</div>
+			<div className={`${styles.cartRow} ${styles.cartSummaryRow}`}>
+				<p>Do zapłaty:</p>
+				<p>
+					{totalCost}
+					{currencySign}
+				</p>
+			</div>
+
+			<FullWidthButton isBlack={true}>Do kasy</FullWidthButton>
+
+			<div className={styles.deliveryInfo}>
+				<img src={CAR_ICON} alt="Car Icon" />
+				<p>
+					Darmowa dostawa od {minSumForFreeDelivery}
+					{currencySign}
+				</p>
+			</div>
+		</div>
+	);
+}
--- front-end/src/views/Payment/Payment.jsx
+++ front-end/src/views/Payment/Payment.jsx
@@ -1,12 +1,17 @@
 import { FormShipment } from "../../components/FormShipment/FormShipment";
 import { FlexContainer } from "../../components/FlexContainer/FlexContainer";
 import { FormPaymanet } from "../../components/FormPayment/FormPayment";
+import { CartContext } from "../../contexts/CartContext";
+import { useContext } from "react";
+import { PaymentSummary } from "../../components/PaymentSummary/PaymentSummary";
 
 export function Payment() {
+	const [cartItems] = useContext(CartContext);
 	return (
 		<FlexContainer>
 			<FormShipment />
 			<FormPaymanet />
+			<PaymentSummary products={cartItems} />
 		</FlexContainer>
 	);
 }
