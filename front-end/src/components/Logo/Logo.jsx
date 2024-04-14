d3fbeebe8853fe806414c243fd5de95eb3024637
Grzegorz <gkopacki92@gmail.com>
2024-04-14 23:52:51 +0200

    changed logo

--- front-end/src/components/Logo/Logo.jsx
+++ front-end/src/components/Logo/Logo.jsx
@@ -1,5 +1,13 @@
 import styles from "./Logo.module.css";
+import  LOGO  from "../../assets/logo.svg";
 
 export function Logo() {
-    return <h1 className={styles.logo}>TopSklep®</h1>;
+	return <>
+    <h1 className={styles.logo}>
+			WEARHOUSE
+         <img src={LOGO} />
+		</h1>
+    </>
+		
+	
 }
