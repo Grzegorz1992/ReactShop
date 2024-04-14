ee1ffa6b434db8d9a7c1b405fc7218cc89fc72c7
Grzegorz <gkopacki92@gmail.com>
2024-04-14 23:46:52 +0200

    developed footer

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -1,9 +1,52 @@
 import styles from "./Footer.module.css";
 
 export function Footer() {
-    return (
-        <div className={styles.footer}>
-            <p>Copyright 2023</p>
-        </div>
-    );
+	return (
+		<>
+			<div className={styles.footer}>
+				<div className={styles.ulSection}>
+					<div className={styles.ulContainer}>
+						<h2 className={styles.heading}>WEARHOUSE</h2>
+						<ul className={styles.footerList}>
+							<div className={styles.liContainer}>
+								<li>O nas</li>
+								<li>Praca</li>
+								<li>Dla mediów</li>
+								<li>Polityka prywatności</li>
+								<li>Nasze sklepy</li>
+								<li>Kontakt</li>
+							</div>
+							<div className={styles.liContainer}>
+								<li>Regulamin zakupów</li>
+								<li>Regulamin kart podarunkowych</li>
+								<li>Zapisz się do Newslettea</li>
+								<li>Regulamin Newslettera</li>
+							</div>
+						</ul>
+					</div>
+
+					<div className={styles.ulContainer}>
+						<h2 className={styles.heading}>OBSŁUGA KLIENTA</h2>
+						<ul className={styles.footerList}>
+							<div className={styles.liContainer}>
+								<li>Dostawa i płatności</li>
+								<li>Zwroty</li>
+								<li>FAQ - produkty</li>
+							</div>
+							<div className={styles.liContainer}>
+								<li>FAQ - sklep</li>
+								<li>Jak pakujemy prezenty</li>
+								<li>Dział reklamacji</li>
+							</div>
+						</ul>
+					</div>
+				</div>
+
+				<p className={styles.footerEnding}>
+					© 2024 WEARHOUSE - korzystając z serwisu WEARHOUSE akceptujesz jego
+					regulamin{" "}
+				</p>
+			</div>
+		</>
+	);
 }
