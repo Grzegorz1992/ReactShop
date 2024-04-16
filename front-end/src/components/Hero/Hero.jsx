d0334efc40341e786bb92cc3ca32c741f7abbb02
Grzegorz <gkopacki92@gmail.com>
2024-04-16 12:11:22 +0200

    changed styles for Form components

--- front-end/src/components/Hero/Hero.jsx
+++ front-end/src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import style from "./Hero.module.css";
+import styles from "./Hero.module.css";
 import { CenteredContent } from "../CenteredContent/CenteredContent";
 import { FullWidthButton } from "../FullWidthButton/FullWidthButton";
 import { NavLink, useParams } from "react-router-dom";
@@ -7,12 +7,14 @@ export function Hero({ heroImage }) {
 	const params = useParams();
 	return (
 		<div
-			className={style.hero}
+			className={styles.hero}
 			style={{ backgroundImage: `url(${heroImage})` }}
 		>
 			<CenteredContent>
-				<div className={style.contentWrapper}>
-					<h2 className={style.heading}>Letnie promocje do <span className={style.promo}>-70%</span></h2>
+				<div className={styles.contentWrapper}>
+					<h2 className={styles.heading}>
+						Letnie promocje do <span className={styles.promo}>-70%</span>
+					</h2>
 					<p>Tylko najlepsze okazje</p>
 					<NavLink to={`/${params.gender}/odziez`}>
 						<FullWidthButton>Sprawdź produkty</FullWidthButton>
